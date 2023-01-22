ec7f625071863c9573d2326de03806ef3127cef9
Pradeep <pradeepmaurya1606@gmail.com>
2023-01-22 14:58:06 +0530

    changes on about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>ABout Us</h1>
+        <h1>ABout Us
+          hello this was hosted on github ok
+        </h1>
       </div>
     )
   }
